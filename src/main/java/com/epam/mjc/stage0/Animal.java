a3b4970a0603de833533dd46ad1717bd0080e8ce
Artur Kosmovskij <kosmos146139@gmail.com>
2024-03-30 15:16:53 +0300

    13

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,33 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
 
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+        String s = "This animal is mostly " + color + ". It has " + numberOfPaws;
+
+        if (numberOfPaws > 1)
+        {
+            s+=" paws and ";
+        }
+        else{
+            s+=" paw and ";
+        }
+        if (hasFur){
+            s += "a fur.";
+        }
+        else {
+            s += "no fur.";
+        }
+        return s;
+    }
 }
